9eebfa28caff13224ea7ee101748fb5c04750d5c
Akshat Chauhan <engineeringstudy.2028@gmail.com>
2025-06-25 15:38:35 +0530

    recursion

---
+++ lesson10/recursion.py
@@ -0,0 +1,16 @@
+#when a functions calls itself is knowkn as recursive fxn
+
+# def add_one(num):
+#     if(num>=9):
+#         return num+1
+    
+#     total=num+1
+#     print(total)
+    
+#     return add_one(total)
+
+# result= add_one(0)
+# print("finalresult:", result )
+
+
+
